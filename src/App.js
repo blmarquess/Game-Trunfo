264dab2a094c795f6ed38a6effa903660d3da08a
Bruno <blmarques.dev@gmail.com>
2021-11-05 15:25:22 -0300

    Testes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ export default class App extends React.Component {
         <div className="w-2/5">
           <Form
             className="text-gray-700"
-            cardName=""
-            cardDescription=""
-            cardAttr1=""
-            cardAttr2=""
-            cardAttr3=""
-            cardImage=""
-            cardRare=""
-            cardTrunfo=""
-            hasTrunfo=""
-            isSaveButtonDisabled=""
-            onInputChange=""
-            onSaveButtonClick=""
+            cardName="Carta"
+            cardDescription="Supre carta"
+            cardAttr1="10"
+            cardAttr2="10"
+            cardAttr3="54"
+            cardImage="dasdasdasd"
+            cardRare="super Rara"
+            cardTrunfo="false"
+            hasTrunfo="false"
+            isSaveButtonDisabled="true"
+            onInputChange="true"
+            onSaveButtonClick="true"
           />
         </div>
         <div className="w-2/5">
