c5e937b6753613f49779d044856f1820e3c13168
Bruno <blmarques.dev@gmail.com>
2021-11-09 03:48:04 -0300

    Fix: Lint for new test evaluator

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Filters extends React.Component {
-
+  render() {
+    const { cardName } = this.props;
+    return (
+      <div>
+        Filter
+        { cardName}
+      </div>
+    );
+  }
 }
 
 Filters.propTypes = {
